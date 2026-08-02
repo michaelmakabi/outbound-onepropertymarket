d32abc4d93f322e410da1eb4c0b07df3648a4319
Michael Makabi <mtip@hey.com>
2026-08-02 16:06:03 -0400

    Wrap app in global FilterProvider (synced date range)

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,15 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import { AuthProvider } from './lib/auth';
+import { FilterProvider } from './lib/filters';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <FilterProvider>
+          <App />
+        </FilterProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
